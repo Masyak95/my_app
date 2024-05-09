188fdea58fabbfbd9b75f5a997b7e76d1bed0d7c
Maryia <97767940+Masyak95@users.noreply.github.com>
2024-05-09 18:42:05 +0200

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/pages/first_page.dart';
-import 'package:my_app/pages/second_page.dart';
+import 'package:my_app/pages/home_page.dart';
+import 'package:my_app/pages/settings_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       home: FirstPage(),
       routes: {
         "/firstPage": (context) => FirstPage(),
-        "/secondPage": (context) => SecondPage(),
+        "/homepage": (context) => HomePage(),
+        "/settingspage": (context) => SettingsPage(),
       },
     );
   }
