90e46510fc4c7d3fd439bff19e4587296f255c86
Maryia <97767940+Masyak95@users.noreply.github.com>
2024-05-09 17:11:13 +0200

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:my_app/pages/first_page.dart';
+import 'package:my_app/pages/second_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
-
-   void userTapped(){
-     print("tap");
-   }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-       body: Center(
-         child: GestureDetector(
-           onTap: userTapped,
-           child: Container(
-             width: 300,
-             height: 300,
-             color: Colors.blue,
-             child: Center(child: Text("Tap me!")),
-           ),
-         ),
-       ),
-      ),
+      home: FirstPage(),
+      routes: {
+        "/firstPage": (context) => FirstPage(),
+        "/secondPage": (context) => SecondPage(),
+      },
     );
   }
 }
