a8700e9e3c7a1109cc046e358777522053bb748f
Maryia <97767940+Masyak95@users.noreply.github.com>
2024-05-09 16:13:35 +0200

    ListView, builder

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
+
+  List names = ["John", "Joy", "Marian"];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            Container(
-              color: Colors.deepPurple,
-              width: 350,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 350,
-            ),
-            Container(
-              color: Colors.green,
-              width: 350,
-            ),
-          ],
+        body: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) => ListTile(
+            title:Text(names[index]),
+          ),
         ),
       ),
     );
