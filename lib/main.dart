9a98c21b7bccd13795dcfd895589a6760136f8fa
Maryia <97767940+Masyak95@users.noreply.github.com>
2024-05-09 15:56:17 +0200

    expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,40 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple,
-          leading: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          title: Text(
-            "My app bar",
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: <Widget>[ // Исправлено: actions должен быть списком виджетов
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.logout),
-              color: Colors.white,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple,
+              ),
+            ),Expanded(
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),Expanded(
+              child: Container(
+                color: Colors.green,
+              ),
             ),
           ],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: EdgeInsets.all(25),
-            child: const Icon(
-              Icons.favorite,
-              color: Colors.white,
-              size: 32,
-            ),
-          ),
         ),
       ),
     );
